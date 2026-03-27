3c12faffd823560af0c16fbd393d2ddb2bd371a0
coderlawa <5d12tpff@students.codeinstitute.net>
2026-03-27 21:03:02 +0000

    docustring function properly commented out

--- run.py
+++ run.py
@@ -167,13 +167,13 @@ print("Welcome to Love Sandwiches Data Automation\n")
 main()
 # stock_data = main()
 
-"""
-def get_stock_value(data):
-    headings = SHEET.worksheet("stock").get_all_values()[0]
-    print("Make the following numbers of sandwiches for next market:\n")
+# """
+# def get_stock_value(data):
+#     headings = SHEET.worksheet("stock").get_all_values()[0]
+#     print("Make the following numbers of sandwiches for next market:\n")
 
-    return {heading: data for heading, data in zip(headings, data)}
+#     return {heading: data for heading, data in zip(headings, data)}
 
-stock_values = get_stock_value(stock_data)
-print(stock_values)
-"""
+# stock_values = get_stock_value(stock_data)
+# print(stock_values)
+# """
